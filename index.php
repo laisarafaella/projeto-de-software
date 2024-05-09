10fdbab6d53260af354fe7afef7b3d1e99dddc6c
laisarafaella <124882756+laisarafaella@users.noreply.github.com>
2024-05-09 14:51:33 -0300

    update landing page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Laravel </title>
+        <title> Site Esportivo </title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
@@ -159,8 +159,10 @@
             <a style="text-decoration: none;">PROGRAMAS</a>
         </li>
         <li>
-            <!--<button class="login">LOGIN</button>-->
-            <a style="text-decoration: none;" class="btnlogin" href="registro.php">LOGIN</a>
+            <a style="text-decoration: none;" class="login" href="registro.php">LOGIN</a>
+        </li>
+        <li>
+            <a style="text-decoration: none;" class="login" href="perfil.php">PERFIL</a>
         </li>
         </ul>
     </div>
